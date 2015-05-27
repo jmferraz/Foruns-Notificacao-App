ac33d57cfb8430e800cb601b1640c1908f4e4a98
Mateus Gondim R. Batista <gondim.mateus@gmail.com>
2015-05-27 13:59:52 -0300

    Browsing up to course page and getting forum links

--- app/src/main/java/com/example/moodleifpe/IFPEService.java
+++ app/src/main/java/com/example/moodleifpe/IFPEService.java
@@ -4,7 +4,9 @@ import retrofit.Callback;
 import retrofit.client.Response;
 import retrofit.http.Field;
 import retrofit.http.FormUrlEncoded;
+import retrofit.http.GET;
 import retrofit.http.POST;
+import retrofit.http.Query;
 
 /**
  * Created by mateus on 15/05/15.
@@ -16,4 +18,7 @@ public interface IFPEService {
     void auth(@Field("username") String username, @Field("password") String password,
               Callback<Response>
                       cb);
+
+    @GET("/course/view.php")
+    void getCourse(@Query("id") String courseId, Callback<Response> cb);
 }
--- app/src/main/java/com/example/moodleifpe/MainActivity.java
+++ app/src/main/java/com/example/moodleifpe/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.moodleifpe;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,6 +20,8 @@ import java.io.InputStreamReader;
 import java.net.CookieHandler;
 import java.net.CookieManager;
 import java.net.CookiePolicy;
+import java.util.ArrayList;
+import java.util.List;
 
 import retrofit.Callback;
 import retrofit.RestAdapter;
@@ -20,51 +29,39 @@ import retrofit.RetrofitError;
 import retrofit.client.Response;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
+    private ProgressBar progressBar;
 
     private static final String ENDPOINT = "http://dead2.ifpe.edu.br/moodle";
     private static final String USERNAME = "estudantevisitante";
     private static final String PASSWORD = "2Patos";
 
+    private final IFPEService service;
+
+    public MainActivity() {
+        super();
+        RestAdapter restAdapter = new RestAdapter.Builder().setEndpoint(ENDPOINT).build();
+        service = restAdapter.create(IFPEService.class);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
         textView = (TextView) findViewById(R.id.text);
         textView.setMovementMethod(new ScrollingMovementMethod());
 
-
         enableCookies();
-
-        RestAdapter restAdapter = new RestAdapter.Builder().setEndpoint(ENDPOINT).build();
-        IFPEService service = restAdapter.create(IFPEService.class);
 
         service.auth(USERNAME, PASSWORD, new Callback<Response>() {
             @Override
             public void success(Response result, Response response) {
-                //Try to get response body
-                BufferedReader reader = null;
-                StringBuilder sb = new StringBuilder();
-                try {
-                    reader = new BufferedReader(new InputStreamReader(result.getBody().in()));
-                    String line;
-                    try {
-                        while ((line = reader.readLine()) != null) {
-                            sb.append(line);
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                String resultString = sb.toString();
-
-                textView.setText(resultString);
+                String resultString = convertToString(result);
+                browseIndexPage(resultString);
             }
 
             @Override
@@ -72,6 +69,78 @@ public class MainActivity extends Activity {
                 textView.setText("Error: " + retrofitError.getKind());
             }
         });
+    }
+
+    private void browseIndexPage(String resultString) {
+        Document document = Jsoup.parse(resultString);
+        Elements elements = document.getElementsByClass("type_course");
+        List<Course> courses = new ArrayList<Course>();
+        for (Element courseElement : elements) {
+            Elements links = courseElement.getElementsByTag("a");
+            if (links.isEmpty()) {
+                continue;
+            }
+            String title = links.first().attr("title");
+            String link = links.first().attr("href");
+            String id = link.split("\\?id=")[1];
+
+            textView.append(title + " - " + id + "\n");
+
+            service.getCourse(id, new Callback<Response>() {
+                @Override
+                public void success(Response result, Response response) {
+                    browseCoursePage(result);
+                }
+
+                @Override
+                public void failure(RetrofitError error) {
+                    textView.append("Failure: " + error.getMessage() + "\n");
+                }
+            });
+
+            Course course = new Course();
+            course.setTitle(title);
+            course.setLink(link);
+            courses.add(course);
+
+        }
+    }
+
+    private void browseCoursePage(Response result) {
+        //TODO move to an appropriate place
+        progressBar.setVisibility(View.INVISIBLE);
+
+        String resultString = convertToString(result);
+        Document document = Jsoup.parse(resultString);
+        Elements elements = document.getElementsByClass("forum");
+        for (Element forum : elements) {
+            Element forumTitle = forum.getElementsByClass("instancename").first();
+            String link = forum.getElementsByTag("a").first().attr("href");
+            if (forumTitle != null) {
+                textView.append(forumTitle.ownText() + "\n");
+                textView.append(link + "\n\n");
+            }
+        }
+    }
+
+    private String convertToString(Response result) {
+        BufferedReader reader;
+        StringBuilder sb = new StringBuilder();
+        try {
+            reader = new BufferedReader(new InputStreamReader(result.getBody().in()));
+            String line;
+            try {
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
     }
 
     private void enableCookies() {
